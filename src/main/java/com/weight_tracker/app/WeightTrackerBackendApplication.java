5ca7d6e98f0e9cf12a91def4eb618ba1b013fbf5
Keita <herausforderer09@gmail.com>
2025-04-21 21:44:18 +0900

    Initial project setup

---
+++ src/main/java/com/weight_tracker/app/WeightTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.weight_tracker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weight_tracker/app/WeightTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weight_tracker.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
